aa979f72acaf3e315c2879d227f23d8ebde66a41
Lyco <zdenek.belehradek@seznam.cz>
2020-11-10 23:45:35 +0100

    data output

--- src/main.rs
+++ src/main.rs
@@ -7,100 +7,123 @@ use cursive::view::{IntoBoxedView, ViewWrapper};
 use cursive::align::{Align, HAlign};
 use cursive::wrap_impl;
 
-struct ShipHull {
-    class: String,
-    size: String,
-    weapons: Vec<String>,
+struct Weapon {
+    view: ResizedView<EditView>
+}
+
+impl Weapon {
+    fn new(name: &str) -> Self {
+        Weapon { view: EditView::new().content(name).fixed_width(15) }
+    }
+
+    fn serialize(&self) -> String {
+        let x = self.view.get_inner().get_content();
+        format!("\"{}\"", x.to_string())
+    }
+}
+
+impl ViewWrapper for Weapon {
+    wrap_impl!(self.view: ResizedView<EditView>);
+}
+
+
+struct Hull {
     view: LinearLayout,
 }
 
-impl ShipHull {
-    fn new(class: impl Into<String> + Clone, size: impl Into<String> + Clone, weapons: Vec<String>) -> Self {
-        let mut equipment_view = LinearLayout::horizontal();
-        for weapon in &weapons {
-            equipment_view.add_child(TextView::new(weapon).align(Align::center()).full_width())
+impl Hull {
+    fn new(name: &str, weapons: Vec<Weapon>) -> Self {
+        let mut weapons_layout = LinearLayout::horizontal();
+        for w in weapons {
+            weapons_layout.add_child(w);
         }
 
-        let hull_view = LinearLayout::vertical()
-            .child(TextView::new(format!("Hull: {}-class {}", class.clone().into(), size.clone().into())))
-            .child(equipment_view);
+        let title_layout = LinearLayout::horizontal()
+            .child(TextView::new("Hull class: "))
+            .child(EditView::new().content(name).full_width());
 
-        Self { class: class.into(), size: size.into(), weapons, view: hull_view }
+        let view = LinearLayout::vertical()
+            .child(title_layout)
+            .child(weapons_layout);
+
+        Self { view }
+    }
+
+    fn serialize(&self) -> String {
+        let title_view = self.view.get_child(0).unwrap().as_any().downcast_ref::<LinearLayout>().unwrap();
+        let weapons_view = self.view.get_child(1).unwrap().as_any().downcast_ref::<LinearLayout>().unwrap();
+
+        let hull_class_view = title_view.get_child(1).unwrap().as_any().downcast_ref::<ResizedView<EditView>>().unwrap().get_inner();
+        let name = hull_class_view.get_content().to_string();
+
+        let mut weapons = Vec::new();
+        for i in 0..weapons_view.len() {
+            let weapon = weapons_view.get_child(i).unwrap().as_any().downcast_ref::<Weapon>().unwrap();
+            weapons.push(weapon);
+        }
+        let weapons = weapons.iter().map(|i| i.serialize()).collect::<Vec<String>>().join(", ");
+        format!("{}: ({})", name, weapons)
     }
 }
 
-impl Clone for ShipHull {
-    fn clone(&self) -> Self {
-        Self::new(&self.class, &self.size, self.weapons.clone())
-    }
-}
-
-impl ViewWrapper for ShipHull {
+impl ViewWrapper for Hull {
     wrap_impl!(self.view: LinearLayout);
 }
 
 struct Ship {
-    name: String,
-    hull: ShipHull,
-    view: Panel<ShipHull>,
+    view: Panel<LinearLayout>,
 }
 
 impl Ship {
-    fn new(name: impl Into<String> + Clone, hull: ShipHull) -> Self {
-        let view = Panel::new(hull.clone())
-            .title(name.clone())
-            .title_position(HAlign::Left);
-        Ship { name: name.into(), hull, view }
+    fn new(name: &str, hull: Hull) -> Self {
+        let title_view = LinearLayout::horizontal()
+            .child(TextView::new("Name: "))
+            .child(EditView::new().content(name).full_width());
+
+        let ship_view = LinearLayout::vertical()
+            .child(title_view)
+            .child(TextView::new("Alpha: the Sigma-class Frigate"))
+            .child(hull);
+
+        let view = Panel::new(ship_view).title("Ship").title_position(HAlign::Left);
+
+        Self { view }
     }
 
-    fn dialog_add(ctx: &mut Cursive) {
-        fn ok(ctx: &mut Cursive, name: &str) {
-            ctx.call_on_name("Ship list", |list: &mut ListView| {
-                //list.add_child(TextView::new(name));
-                list.add_child("test", TextView::new("test"));
-            });
-            ctx.pop_layer();
-        }
+    fn serialize(&self) -> String {
+        let title_view = self.view.get_inner().get_child(0).unwrap().as_any().downcast_ref::<LinearLayout>().unwrap();
+        let hull_view = self.view.get_inner().get_child(2).unwrap().as_any().downcast_ref::<Hull>().unwrap();
 
-        ctx.add_layer(
-            Dialog::around(
-                EditView::new()
-                    .on_submit(ok)
-                    .with_name("Add ship dialog")
-            )
-                .title("Add ship")
-                .button("Add", |ctx| {
-                    let new_ship_name = ctx.call_on_name("Add ship dialog", |data: &mut EditView| { data.get_content() }).unwrap();
-                    ok(ctx, &new_ship_name)
-                })
-                .dismiss_button("Cancel")
-        )
-    }
-}
+        let name = title_view.get_child(1).unwrap().as_any().downcast_ref::<ResizedView<EditView>>().unwrap().get_inner().get_content().to_string();
+        let hull = hull_view.serialize();
 
-impl Clone for Ship {
-    fn clone(&self) -> Self {
-        Self::new(&self.name, self.hull.clone())
+        format!("Ship: Name: {}, Hull: {})", name, hull)
     }
 }
 
 impl ViewWrapper for Ship {
-    wrap_impl!(self.view: Panel<ShipHull>);
+    wrap_impl!(self.view: Panel<LinearLayout>);
 }
 
 fn main() {
     let mut ctx = cursive::default();
 
+    let root_window = Ship::new(
+        "My Ship",
+        Hull::new(
+            "HullName",
+            vec![Weapon::new("Weapon1"), Weapon::new("Weapon2")],
+        ),
+    ).with_name("root");
+
     ctx.add_global_callback('q', |s| s.quit());
 
     ctx.add_layer(LinearLayout::vertical()
-        .child(Ship::new("Weaponship1", ShipHull::new("Alpha", "Frigate", vec!["Weapon".to_string()])))
-        .child(Ship::new("Weaponship2", ShipHull::new("Beta", "Frigate", vec!["Weapon1".to_string(), "Weapon2".to_string(), "Weapon3".to_string()])))
-        .child(Ship::new("Weaponship3", ShipHull::new("Gamma", "Frigate", vec!["WeaponX".to_string(), "WeaponY".to_string()])))
-        .child(Button::new("Add ship", Ship::dialog_add))
-        .with_name("Ship list")
-        .full_width()
+        .child(root_window)
+        .child(Button::new("Quit", |s| s.quit()))
     );
 
     ctx.run();
+
+    ctx.call_on_name("root", |root: &mut Ship| { println!("{}", root.serialize()) });
 }
